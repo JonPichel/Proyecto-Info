6195aa5ce421d9f3f30aca3d871d173995454532
ADVAGA1 <64835607+ADVAGA1@users.noreply.github.com>
2020-05-09 18:23:52 +0200

    Update flight.py

--- flight.py
+++ flight.py
@@ -155,7 +155,7 @@ def check_overlap_list(f, vector_flight):
     f: Flight, a Flight object
     vector_flight: list, a list of Flight objects
     Return: bool, False if they don't overlap, True if they do
-    Created by Jonathan Pichel on April 5rd 2020
+    Created by Jonathan Pichel on May 5rd 2020
     """
     for flight in vector_flight:
         if check_overlap(f, flight):
@@ -168,7 +168,7 @@ def check_inner_overlap(vector_flight):
     Checks if there exists any overlap between the flights of a list
     vector_flight: list, a list of Flight objects
     Return: bool, True if there's any overlap, False otherwise
-    Created by Jonathan Pichel on April 5rd 2020
+    Created by Jonathan Pichel on May 5rd 2020
     """
     for i in range(len(vector_flight)):
         if check_overlap_list(vector_flight[i], vector_flight[i+1:]):
@@ -181,10 +181,11 @@ def plot_flight(f, show=True):
     Plots a flight
     f: Flight, a Flight object
     show: bool, flag to control whether to show the plot or not. True by default.
-    Created by Pol Roca on April 6th 2020
+    Created by Pol Roca on May 6th 2020
+    Tested by Adrià Vaquer on May 9th 2020
     """
     # Plot the flight
-    plt.barh(f"{f.arr}\n{f.dep}", flight_duration(f), left=f.time_dep, color='lightblue')
+    plt.barh(f"{f.dep}\n{f.arr}", flight_duration(f), left=f.time_dep, color='lightblue')
     
     # Plot customization
     # Set the ticks and give them labels
