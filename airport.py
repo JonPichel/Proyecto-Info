81b5912946659c20764c7790da3db4c8ca31ef57
Jonathan Pichel <jonathanpc@protonmail.com>
2020-05-11 22:22:51 +0200

    Added show_airport for testing

--- airport.py
+++ airport.py
@@ -27,7 +27,6 @@ def set_ap_info(ap, s1, s2, s3):
         print("Wrong parameters. Provide an Airport object.")
         return False
 
-
 def set_costs(ap, c1, c2, c3):
     """ Function set_costs(ap, c1, c2, c3)
     ==================================================
@@ -65,18 +64,57 @@ def read_airports(f):
             try:
                 code = words[0]
                 location = words[1]
-                name = words[2:]
+                name = " ".join(words[2:])
             except IndexError:
                 print(f"Wrong format at line {i + 1}.")
             # If there was no errors, add the airport
             else:
                 a = Airport()
-                print(i)
                 set_ap_info(a, code, name, location)
                 airports.append(a)
 
     return airports
 
+def show_airport(ap):
+    """ Function show_airport (ap: Airport)
+    ==================================================
+    Prints on the screen the information about an airport
+    ap: Airport, the Airport which data will be printed
+    Created by Jonathan Pichel on April 11th 2020
+    """
+    try:
+        code = ap.code
+        name = ap.name
+        location = ap.location
+        fees = ap.fees
+        free_hours = ap.free_hours
+        cost_per_hour = ap.cost_per_hour
+    except AttributeError:
+        print("Wrong parameters. Provide an Airport object.")
+        return
+   	
+    # Check if some information is missing
+    if not code:
+        code = 'UNKNOWN ICAO CODE'
+    if not name:
+        name = 'UNKNOWN NAME'
+    if location:
+        location = f"{location.split(',')[0]}º N, {location.split(',')[1]}º W"
+    else:
+        location = 'UNKNOWN LOCATION'
+    
+    # If the name contains Airport, we don't add that information
+    if "Airport" in name:
+        print(f"Information about {name}:")
+    else:
+        print(f"Information about {name} Airport:")
+    print(f"\tICAO code: {code}")
+    print(f"\tCoordinates: {location}")
+    print(f"\tLanding fees: {fees}")
+    print(f"\tFree parking hours: {free_hours}")
+    print(f"\tAditional cost per hour: {cost_per_hour}")
+
+
 if __name__ == '__main__':
     for ap in read_airports('error.txt'):
-        print(ap)
+        show_airport(ap)
